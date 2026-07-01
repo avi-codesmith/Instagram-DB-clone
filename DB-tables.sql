24aabc1b07f7d6b0a5285b00d9148d2042787b7f
avi-codesmith <avi.codesmith@gmail.com>
2026-07-01 15:19:49 +0530

    finish builing schemata

--- DB-tables.sql
+++ DB-tables.sql
@@ -26,3 +26,24 @@ CREATE TABLE comments(
    FOREIGN KEY (photo_id) REFERENCES photos(id)
 );
 
+CREATE TABLE likes(
+   user_id INT NOT NULL,
+   photo_id INT NOT NULL,
+   created_at TIMESTAMP DEFAULT NOW(),
+   FOREIGN KEY (user_id) REFERENCES users(id),
+   FOREIGN KEY (photo_id) REFERENCES photos(id),
+   UNIQUE(user_id, photo_id)
+   -- PRIMARY KEY(user_id, photo_id) -- we can use this as well
+);
+
+CREATE TABLE follows(
+  follower_id INT,
+  followee_id INT,
+  CHECK (follower_id != followee_id),
+  created_at TIMESTAMP DEFAULT NOW(),
+  FOREIGN KEY (follower_id) REFERENCES users(id),
+  FOREIGN KEY (followee_id) REFERENCES users(id),
+  PRIMARY KEY (follower_id, followee_id)
+-- UNIQUE(user_id, photo_id)we can use but 
+-- then we need to add NULl as in ... 30 and 31
+)
